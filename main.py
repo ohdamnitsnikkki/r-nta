7ddcd58f25dadecf8bada0483e045554ca06a068
ohdamnitsnikkki <mammastjernberg@gmail.com>
2025-10-23 08:09:34 +0000

    add code for user to add their taxes

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hi")
+# Startkapital satt till 100'000 kronor
+
+startkapital = 100000
+
+# Be användare fylla i räntesatsen, samt felhantera ogiltigt format
+
+while True:
+    try:
+        # Be användaren om räntesats
+        ranta = float(input("Fyll i din årliga räntesat: "))
+        break # Avsluta om räntesatsen är i korrekt format
+    except ValueError: # Be användaren försöka igen om räntesatsen är felaktig
+        print("Ogiltigt format. Försök igen, exempel 3.5")
